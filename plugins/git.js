13be6d8cec7b90d83a63663583c8e14cb0465804
Aflahxrd <88531428+nexusNw@users.noreply.github.com>
2021-08-22 10:07:29 +0530

    Create git.js

---
+++ plugins/git.js
@@ -0,0 +1,28 @@
+const Asena = require('../events');
+const {MessageType, MessageOptions, Mimetype} = require('@adiwajshing/baileys');
+const axios = require('axios');
+
+const Language = require('../language');
+const Lang = Language.getString('wallpaper');
+
+Asena.addCommand({pattern: 'git', fromMe: false, desc: Lang.WP}, (async (message, match) => {
+
+    var r_text = new Array ();
+    
+    
+   
+  r_text[0] = "https://i.imgur.com/fMYl0re.jpeg";
+    
+    
+    var i = Math.floor(1*Math.random())
+
+    var respoimage = await axios.get(`${r_text[i]}`, { responseType: 'arraybuffer' })
+
+    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: `*creater Aflah*
+*owner number wa.me/9181296243***
+*bot setting video : https://youtu.be/VR_ppqEl2q8*
+*githublink       https://github.com/nexusNw/filo*
+*audio commads     https://github.com/nexusNw/filo/tree/master/uploads*
+`}) 
+
+}));
