3b952b73a393fce24f998ca3eed8ec4c25c687bc
Aflahxrd <88531428+nexusNw@users.noreply.github.com>
2021-08-23 19:16:21 +0530

    Update mod.js

--- plugins/mod.js
+++ plugins/mod.js
@@ -19,20 +19,13 @@ Asena.addCommand({pattern: 'mod', fromMe: false, desc: Lang.WP}, (async (message
     var respoimage = await axios.get(`${r_text[i]}`, { responseType: 'arraybuffer' })
 
     await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: `*short list*
-*https://github.com/nexusNw/filo/*         
-          ```:-owned by nexusNw```
-*https://github.com/cyberchekuthan/Amalser_v1/*
-             ```owned by Amalser```
-*https://github.com/farhan-dqz/JulieMwol/*
-          ```owned by Farhan-dqz```
-*https://github.com/saidalisaid2/Liyamol/*
-           ```owned by saidali```
-*https://github.com/J-I-H-A-D/ElsaMwol/*
-          ```owned by Jihad```
-*https://github.com/afnanplk/lastpink*
-               ```owned by Afnan plk```
-*https://github.com/ameer-kallumthodi/pikachu*
-           ```owned by ameer kallumthodi```
+*https://github.com/nexusNw/filo/* -owned by nexusNw
+*https://github.com/cyberchekuthan/Amalser_v1/* -owned by Amalser
+*https://github.com/farhan-dqz/JulieMwol/* -owned by Farhan-dqz
+*https://github.com/saidalisaid2/Liyamol/* -owned by saidali
+*https://github.com/J-I-H-A-D/ElsaMwol/* -owned by Jihad
+*https://github.com/afnanplk/lastpink* -owned by Afnan plk
+*https://github.com/ameer-kallumthodi/pikachu* -owned by ameer kallumthodi
       ~*its just a small list.to more gits available 🙂*~
 `}) 
 
