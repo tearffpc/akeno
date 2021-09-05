c53a34fd8c4d395238f3cee51c07b81744baa002
Aflahxrd <88531428+nexusNw@users.noreply.github.com>
2021-09-05 19:30:24 +0530

    Update git.js

--- plugins/git.js
+++ plugins/git.js
@@ -11,7 +11,7 @@ Asena.addCommand({pattern: 'git', fromMe: false, desc: Lang.WP}, (async (message
     
     
    
-  r_text[0] = "https://i.imgur.com/fMYl0re.jpeg";
+  r_text[0] = "https://i.imgur.com/1hGpu9y.jpeg";
     
     
     var i = Math.floor(1*Math.random())
