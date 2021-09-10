05eea4827492177957052d7087d2177429cbd415
Tearless <90409549+tearffpc@users.noreply.github.com>
2021-09-10 14:50:08 +0530

    Update filters.js

--- plugins/filters.js
+++ plugins/filters.js
@@ -177,10 +177,10 @@ Asena.addCommand({on: 'text', fromMe: false}, (async (message, match) => {
     if(Config.STICKERP){
     let banned = jid.find( Jid => Jid === message.jid);
     if(banned !== undefined) return
-    if (!!message.mention && message.mention[0] == '918129624395@s.whatsapp.net') {
-await message.client.sendMessage(message.jid, fs.readFileSync('./stickers/Mention.webp'), MessageType.sticker, { mimetype: Mimetype.webp, quoted : message.data, ptt: false})
+    if (!!message.mention && message.mention[0] == '919562881715@s.whatsapp.net') {
+await message.client.sendMessage(message.jid, fs.readFileSync('./stickers/filo.webp'), MessageType.sticker, { mimetype: Mimetype.webp, quoted : message.data, ptt: false})
     }
-const array = ['Dheeran','nw akhil','filo','navak','Athul','pain','Leroi','nexusnw']
+const array = ['Dheeran','nw akhil','navak','Athul','pain','Leroi','nexusnw']
 array.map( async (a) => {
 let pattern = new RegExp(`\\b${a}\\b`, 'g');
 if(pattern.test(message.message)){
