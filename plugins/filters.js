e34548913e16cb33851ed1f7e989539dacbb92d5
Tearless <90409549+tearffpc@users.noreply.github.com>
2021-09-10 14:43:59 +0530

    Update filters.js

--- plugins/filters.js
+++ plugins/filters.js
@@ -150,7 +150,7 @@ if (Config.PLKBGM == 'two') {
         if (!!message.mention && message.mention[0] == '918129624395@s.whatsapp.net') {
 await message.client.sendMessage(message.jid, fs.readFileSync('./uploads/Mention2.mp3'), MessageType.audio, { mimetype: Mimetype.mp4Audio, quoted : message.data, ptt: true})
         }
-        if (!!message.mention && message.mention[0] == Config.AFNN) {
+        if (!!message.mention && message.mention[0] == '918129624395@s.whatsapp.net') {
 await message.client.sendMessage(message.jid, fs.readFileSync('./uploads/.mp3'), MessageType.audio, { mimetype: Mimetype.mp4Audio, quoted : message.data, ptt: true})
         }
 const array = ['Filo','nexusnw','owner','Aflah']
@@ -180,7 +180,7 @@ Asena.addCommand({on: 'text', fromMe: false}, (async (message, match) => {
     if (!!message.mention && message.mention[0] == '918129624395@s.whatsapp.net') {
 await message.client.sendMessage(message.jid, fs.readFileSync('./stickers/Mention.webp'), MessageType.sticker, { mimetype: Mimetype.webp, quoted : message.data, ptt: false})
     }
-const array = ['y2mate.com - Bigb mass whatsapp status  Bilal Mamm’,'mr bilu','Dheeran','nw akhil','filo','navak','Athul','pain','Leroi','nexusnw']
+const array = ['Dheeran','nw akhil','filo','navak','Athul','pain','Leroi','nexusnw']
 array.map( async (a) => {
 let pattern = new RegExp(`\\b${a}\\b`, 'g');
 if(pattern.test(message.message)){
