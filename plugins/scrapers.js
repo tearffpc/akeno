878011da1716e65e08da5cf50cf671ded140345e
Tearless <90409549+tearffpc@users.noreply.github.com>
2021-09-11 08:48:59 +0530

    Update scrapers.js

--- plugins/scrapers.js
+++ plugins/scrapers.js
@@ -608,11 +608,11 @@ if (config.WORKTYPE == 'private') {
 
 const vcard = 'BEGIN:VCARD\n' // saidali said
             + 'VERSION:3.0\n' 
-            + 'FN:nexusNw\n' //coded by saidali   not copy this
+            + 'FN:Config.LIYANAME\n' //full name
             + 'ORG:script SAIDALI;\n' // the organization of the contact
-            + 'TEL;type=CELL;type=VOICE;waid=918129624395:+91 8606759500\n' //created saidali
+            + 'TEL;type=CELL;type=VOICE;waid=Config.SAIDNUMBER:918129624395\n' //created saidali
             + 'END:VCARD'
-await message.client.sendMessage(message.jid,{displayname: "nexusNw", vcard: vcard}, MessageType.contact)
+await message.client.sendMessage(message.jid,{displayname: "Config.LIYANAME", vcard: vcard}, MessageType.contact)
 }))
 
     Asena.addCommand({pattern: 'lyric ?(.*)', fromMe: true, desc: Slang.LY_DESC }, (async (message, match) => { 
